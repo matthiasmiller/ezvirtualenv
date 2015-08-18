2587d4252ab9fad5760ab560baf197456025857c
Matthias Miller <outofhanwell@gmail.com>
2015-08-18 16:22:19 -0600

    more code simplification

--- ezvirtualenv.py
+++ ezvirtualenv.py
@@ -10,10 +10,10 @@ def run_as_virtual():
     script_path = os.path.abspath(sys.modules['__main__'].__file__)
     script_args = sys.argv[1:]
 
-    venv = _VirtualEnvironment(os.path.dirname(script_path))
+    venv = _VirtualEnvironment(script_path, script_args)
     if not venv.in_virtual_env:
         venv.auto_create()
-        venv.run_script(script_path, script_args)
+        venv.run_script()
 
 
 class _VirtualEnvironment(object):
@@ -21,55 +21,53 @@ class _VirtualEnvironment(object):
         Breaking changes may appear in the future.
     """
 
+    # Used for sanity-check to protect against infinite loops.
     _check_environ_name = '__EXPECT_VIRTUAL_ENV'
 
-    def __init__(self, project_dir):
-        self._project_dir = project_dir
-        self._requirements_path = os.path.join(self._project_dir, 'requirements.txt')
+    def __init__(self, script_path, script_args):
+        self._script_dir = os.path.dirname(script_path)
+        self._script_path = script_path
+        self._script_args = script_args
+        self._requirements_path = os.path.join(self._script_dir, 'requirements.txt')
 
-        self._venv_dir = os.path.join(self._project_dir, '.venv')
-        self._venv_cache_path = os.path.join(self._venv_dir, '%s-cache.json' % MODULE_NAME)
+        self._venv_dir = os.path.join(self._script_dir, '.venv')
+        self._venv_cache_path = os.path.join(self._venv_dir, '%s.cache' % MODULE_NAME)
         self._venv_python = os.path.join(self._venv_dir, 'Scripts', os.path.basename(sys.executable))
         self._venv_pip = os.path.join(self._venv_dir, 'Scripts', 'pip')
         self._in_virtual_env = os.path.normcase(self._venv_python) == os.path.normcase(sys.executable)
+
+        # Make sure this is the *right* virtual environment!
+        assert self._in_virtual_env == bool(os.environ.get(self._check_environ_name)), \
+            'Error detecting virtual environment!'
 
     @property
     def in_virtual_env(self):
         return self._in_virtual_env
 
-    def run_script(self, script_path, script_args):
+    def run_script(self):
         assert not self._in_virtual_env
 
         print 'Relaunching in virtual environment...'
 
-        # Sanity-check to protect against infinite loops.
         env = os.environ.copy()
-        assert not env.get(self._check_environ_name), 'Error detecting virtual environment!'
         env[self._check_environ_name] = 'yes'
 
         # Relaunch main script with same arguments.
-        proc = subprocess.Popen([self._venv_python, script_path] + script_args, env=env)
+        proc = subprocess.Popen([self._venv_python, self._script_path] + self._script_args, env=env)
         sys.exit(proc.wait())
 
     def auto_create(self):
         assert not self._in_virtual_env
 
-        # Only import outside of the actual virtual environment.
+        # Only import outside of the virtual environment.
         import virtualenv
 
         if not os.path.isdir(self._venv_dir):
             print 'Creating virtual environment...'
             virtualenv.create_environment(self._venv_dir)
 
-        # Generate a key for requirements and virtualenv file.
-        cache_key = ''
-        for path in (self._requirements_path, os.path.abspath(__file__)):
-            cache_key += ','
-            if os.path.isfile(path):
-                si = os.stat(path)
-                cache_key += '%s-%s' % (si.st_mtime, si.st_size)
-
-        # Skip if the files are unchanged.
+        # Skip if the requirements and ezvirtualenv files are unchanged.
+        cache_key = self._get_cache_key()
         if os.path.isfile(self._venv_cache_path):
             with open(self._venv_cache_path) as f:
                 if cache_key == f.read().strip():
@@ -82,6 +80,16 @@ class _VirtualEnvironment(object):
         with open(self._venv_cache_path, 'w') as f:
             f.write(cache_key)
             
+    def _get_cache_key(self):
+        # Based on modified time + size.
+        cache_key = ''
+        for path in (self._requirements_path, os.path.abspath(__file__)):
+            cache_key += ','
+            if os.path.isfile(path):
+                si = os.stat(path)
+                cache_key += '%s-%s' % (si.st_mtime, si.st_size)
+        return cache_key
+
     def _refresh_requirements(self):
         if os.path.isfile(self._requirements_path):
             # Note that we use the pip executable instead of the module to correctly detect dependencies.
