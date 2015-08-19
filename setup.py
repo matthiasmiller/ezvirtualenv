3dbc8450ea9bb805beea015e395596cb872dcb59
Matthias Miller <outofhanwell@gmail.com>
2015-08-19 07:30:53 -0600

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='ezvirtualenv',
-      version='0.1.0',
+      version='0.1.1',
       description='Easy Virtual Environment',
       author='Matthias Miller',
       url='https://github.com/matthiasmiller/ezvirtualenv',
